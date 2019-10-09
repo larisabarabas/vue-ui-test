8a224107bf63587840329c342a13d46c6ebb6452
StefaniaB <stefaniabarabas@gmail.com>
2019-10-09 13:30:53 +0200

    Initial commit
    Initial setup

---
+++ src/components/NewComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="text-container">
+        <h3>{{ msg }}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"NewComponent",
+    props:{
+        msg:String
+    }
+}
+</script>
+
+<style scoped>
+    .text-container{
+        padding:20px;
+        border: 1px solid goldenrod;
+        background-color: antiquewhite
+    }
+</style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>This is Profile page</h1>
+        <NewComponent msg="Rendering newComponent"/>
+    </div>
+</template>
+
+<script>
+import NewComponent from '@/components/NewComponent.vue';
+export default {
+    name:"Profile",
+    components:{
+        NewComponent
+    }
+}
+</script>
