2fd769f4bf6935c60e9346c708a35f5456e919df
StefaniaB <stefaniabarabas@gmail.com>
2020-01-07 16:09:04 +0100

    Dynamic timeline

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,101 +3,11 @@
     <div>
       <h2>Testing the <b>vertical</b> timeline</h2>
         <hooper :itemsToShow="2" :vertical="true" style="height: 700px" :infiniteScroll="false">
-          <slide>
-            <p class="month">JAN</p>
+          <slide v-for="(item, index) in groupByMonth" :key="index">
+            <p class="month">{{index}}</p>
             <div class="columns is-multiline">
-              <div class="column card">card </div>
-              <div class="column card">card </div>
-              <div class="column card">card </div>
-              <div class="column card">card </div>
-              <div class="column card">card </div>
-              <div class="column card">card </div>
-            </div>
-          </slide>
-          <slide>
-            <p class="month">FEB</p>
-              <div class="columns is-multiline">
-                <div class="column card">card </div>
-                <div class="column card">card </div>
-                <div class="column card">card </div>
-              </div>
-          </slide>
-          <slide>
-            <p class="month">MAR</p>
-            <div class="columns is-multiline">
-                <div class="column card">card </div>
-                <div class="column card">card </div>
-            </div>
-          </slide>
-          <slide>
-            <p class="month">APR</p>
-            <div class="columns is-multiline">
-                <div class="column card">card </div>
-                <div class="column card">card </div>
-                <div class="column card">card </div>
-            </div>
-          </slide>
-          <slide>
-            <p class="month">MAY</p>
-              <div class="columns is-multiline">
-                <div class="column card">card </div>
-                <div class="column card">card </div>
-                <div class="column card">card </div>
-            </div>
-          </slide>
-          <slide>
-            <p class="month">JUN</p>
-              <div class="columns is-multiline">
-                <div class="column card">card </div>
-                <div class="column card">card </div>
-                <div class="column card">card </div>
-            </div>
-          </slide>
-          <slide>
-            <p class="month">JUL</p>
-            <div class="columns is-multiline">
-                <div class="column card">card </div>
-                <div class="column card">card </div>
-                <div class="column card">card </div>
-            </div>
-          </slide>
-          <slide>
-            <p class="month">AUG</p>
-            <div class="columns is-multiline">
-                <div class="column card">card </div>
-                <div class="column card">card </div>
-            </div>
-          </slide>
-          <slide>
-            <p class="month">SEP</p>
-            <div class="columns is-multiline">
-                <div class="column card">card </div>
-                <div class="column card">card </div>
-                <div class="column card">card </div>
-            </div>
-          </slide>
-          <slide>
-            <p class="month">OCT</p>
-            <div class="columns is-multiline">
-                <div class="column card">card </div>
-                <div class="column card">card </div>
-                <div class="column card">card </div>
-            </div>
-          </slide>
-          <slide>
-            <p class="month">NOV</p>
-            <div class="columns is-multiline">
-                <div class="column card">card </div>
-                <div class="column card">card </div>
-                <div class="column card">card </div>
-            </div>
-          </slide>
-          <slide>
-            <p class="month">DEC</p>
-            <div class="columns is-multiline">
-                <div class="column card">card </div>
-                <div class="column card">card </div>
-                <div class="column card">card </div>
+              <div class="column card" v-for="element in item" :key="element.id">card </div>
+
             </div>
           </slide>
         </hooper>
@@ -108,6 +18,8 @@
 <script>
 import { Hooper, Slide } from 'hooper';
 import 'hooper/dist/hooper.css';
+import _ from 'lodash'
+import moment from 'moment'
 
 export default {
   name: 'home',
@@ -120,6 +32,29 @@ export default {
       console.log(i)
       return this.$refs.group1.slideTo(i)
     }
+  },
+  data:()=>{
+    return{
+      projects:[
+        {id:0, name:'Wix Marketing translations part 2', created_date:"2020-01-06", month:'january'},
+        {id:1, name:'Wix Marketing translations part 1', created_date:"2020-01-04",  month:'january'},
+        {id:2, name:'Wix Logistics translations', created_date:"2020-02-06",  month:'february'},
+        {id:3, name:'Wix RMs translations', created_date:"2019-12-06",  month:'december'},
+        {id:4, name:'Wix sss translations', created_date:"2019-12-20",  month:'december'},
+        {id:5, name:'Wix RMddds translations', created_date:"2019-12-12",  month:'december'},
+        {id:6, name:'Wix RdddMs translations', created_date:"2019-12-06",  month:'december'},
+      ]
+    }
+  },
+  computed:{
+    groupByMonth: function (){
+      return _.groupBy(this.projects, (item)=>{
+        return moment(item.created_date).format('MMMM')
+      })
+    }
+  },
+  mounted(){
+    console.log(moment("2020-01-06T08:54:27.000000Z").format('MMMM'))
   }
 }
 </script>
